49a4f68f3d74cf58e3f5f4ee06e1026a512f6efa
Ankur Agarwal <ankur.agarwal@enterprisewide.com>
2023-10-20 00:35:16 +0000

    First commit

---
+++ app/incidents/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'incidents/incidents/test/integration/FirstJourney',
+		'incidents/incidents/test/integration/pages/IncidentsList',
+		'incidents/incidents/test/integration/pages/IncidentsObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, IncidentsList, IncidentsObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('incidents/incidents') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheIncidentsList: IncidentsList,
+					onTheIncidentsObjectPage: IncidentsObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
